b7512d8844771ef2224db94ce0fa3993e93a03f8
SGR08 <fiuba08@gmail.com>
2025-03-25 19:01:28 -0300

    test

--- src/test/java/backend/service/TransferenciaDetalleServiceTest.java
+++ src/test/java/backend/service/TransferenciaDetalleServiceTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class TransaccionDetalleServiceTest {
+class TranscendentalistServiceTest {
 
     @Mock
     private TransferenciaDetalleRepository transfereciaDetalleRepository;
